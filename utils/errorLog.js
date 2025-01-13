974e0f84510347ba558923aea01fb8b0316322d4
= <ricksarkar2005@gmail.com>
2025-01-13 19:31:57 +0530

    Log error modified and send email added

--- utils/errorLog.js
+++ utils/errorLog.js
@@ -1,19 +1,27 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
 
 export const errorLog = async (error) => {
     try {
-        const folderPath = path.join(process.cwd(), '../logs');
+        const folderPath = path.join(process.cwd(), './logs');
         const filePath = path.join(folderPath, 'error.log');
-        if (!fs.existsSync(folderPath)) {
-            fs.mkdirSync(folderPath, { recursive: true });
+
+        try {
+            await fs.access(folderPath);
+        } catch {
+            await fs.mkdir(folderPath, { recursive: true });
         };
-        if (!fs.existsSync(filePath)) {
-            fs.writeFileSync(filePath, '', 'utf8');
+
+        try {
+            await fs.access(filePath);
+        } catch {
+            await fs.writeFile(filePath, '', 'utf8');
         };
-        const errorLog = `Error: Something went wrong. Time: ${new Date().toISOString()}\n` + (error.stack || error.message || 'No stack trace available') + '\n\n';
-        fs.appendFileSync(filePath, errorLog, 'utf8');
+
+        const errorLog = `Error: Something went wrong. Time: ${new Date().toISOString()}\n` + `${error.stack || error.message || 'No stack trace available'}\n\n`;
+        await fs.appendFile(filePath, errorLog, 'utf8');
+
     } catch (logError) {
         console.error('Error logging error:', logError);
-    };
+    }
 };
