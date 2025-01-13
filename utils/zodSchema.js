777839d9e978e9e9e688bab5edf87491178048fb
= <ricksarkar2005@gmail.com>
2025-01-13 18:43:27 +0530

    jwt added and lastName optional maded

--- utils/zodSchema.js
+++ utils/zodSchema.js
@@ -2,7 +2,7 @@ import z from 'zod';
 
 export const signupSchema = z.object({
     firstName: z.string().min(3).max(255),
-    lastName: z.string().min(3).max(255),
+    lastName: z.string().min(3).max(255).optional(),
     email: z.string().email().max(255),
     mobileNumber: z.string().min(10).max(10),
     password: z.string().min(8).max(255),
