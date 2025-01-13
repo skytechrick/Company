b132048889a715a14b79a9601bddc7fdf4fe9f44
= <ricksarkar2005@gmail.com>
2025-01-13 17:51:35 +0530

    Api route ready

--- app.js
+++ app.js
@@ -4,30 +4,18 @@ import fs from 'fs';
 import bodyParser from 'body-parser';
 import admin from "./utils/googleConfig.js";
 import { errorLog } from "./utils/errorLog.js";
+import apiRouter from "./routes/apiRouter.js";
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/files", express.static("./public"));
+app.use("/api/v1/", apiRouter);
 
 app.get('/', async (req, res) => {
     let file = fs.readFileSync('./index.html');
     res.send(file.toString());
-});
-
-// Handle authentication with Firebase token
-app.post('/auth/authenticate', async (req, res) => {
-    const token = req.body.token;
-  
-    try {
-        const decodedToken = await admin.auth().verifyIdToken(token);
-        console.log('User verified:', decodedToken);
-        res.status(200).send({ message: 'User authenticated', user: decodedToken });
-    } catch (error) {
-        console.error('Authentication error:', error);
-        res.status(401).send({ message: 'Authentication failed' });
-    }
 });
 
 app.use( async (err, req, res, next) => {
