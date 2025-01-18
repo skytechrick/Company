e489b1bcce44c3a8f897e5c2f674691a146b0e66
= <ricksarkar2005@gmail.com>
2025-01-18 17:40:21 +0530

    Redirect to home on login

--- controllers/authPageControllers.js
+++ controllers/authPageControllers.js
@@ -1,7 +1,35 @@
 import fs from 'fs';
 import path from 'path';
 
+import { verifyToken } from '../utils/jwtController.js';
+import Modules from "../modules.js";
+
 export const authentication = async ( req , res , next ) => {
-    const folderPath = path.join(process.cwd(), '/pages/authenticationPage.html');
-    return res.status(200).sendFile(folderPath);
+    try {
+
+        const folderPath = path.join(process.cwd(), '/pages/authenticationPage.html');
+        const token = req.signedCookies.token;
+        if (!token) {
+            return res.status(200).clearCookie('token').sendFile(folderPath);
+        };
+        
+        const decoded = verifyToken(token);
+        if (!decoded) {
+            return res.status(200).clearCookie('token').sendFile(folderPath);
+        };
+        
+        const user = await Modules.user.findById( decoded.id );
+        
+        if (!user) {
+            return res.status(200).clearCookie('token').sendFile(folderPath);
+        };
+        
+        if (user.loggedIn.token !== decoded.token) {
+            return res.status(200).clearCookie('token').sendFile(folderPath);
+        };
+        
+        return res.status(302).redirect('/');
+    } catch (error) {
+        next(error);
+    };
 };
